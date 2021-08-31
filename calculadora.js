d50678ae7850c3564203a67f15457bfb5de06d14
sorukenss <i_david1999@hotmail.com>
2021-08-31 10:20:27 -0500

    pruebadesdevisual

--- calculadora.js
+++ calculadora.js
@@ -75,9 +75,18 @@ function clear(){
     opeAnterior = '';
     operacion = undefined;
 }
+//estudiar 
 
 function actualizarDisplay(){
     result.value = opeActual;
 }
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML = this.responseText;
+      }
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
 
 clear();
